2a846043a9883f49cae1cb7276d51d096d0df621
ghghrzg <72688959+ghghrzg@users.noreply.github.com>
2021-11-24 16:17:47 +0100

    Merge branch 'send-module' of C:\Users\lucas\Documents\GitHub\MorseProject with conflicts.

--- src/com/dhbw/MorseProject/send/Encoder.java
+++ src/com/dhbw/MorseProject/send/Encoder.java
@@ -1,4 +1,32 @@
 package com.dhbw.MorseProject.send;
 
 public class Encoder {
+
+    private static Encoder instance = new Encoder();
+
+    private Encoder() {
+
+    }
+
+    public static Encoder getInstance() {
+        return instance;
+    }
+
+    public void send(String morse, Melody melody) {
+        Char[] signals = morse.toCharArray();
+
+        for (char x in signals) {
+
+        }
+    }
+
+    private void shortSignal() {
+
+    }
+
+    private void longSignal() {
+
+    }
+
+
 }
