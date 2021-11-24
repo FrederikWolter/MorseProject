e19100dbf5fbb2c8d428c954c1d932b85bbcc1b9
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2021-11-24 18:32:25 +0100

    Cleaned up test scenario

--- src/com/dhbw/MorseProject/MorseProject.java
+++ src/com/dhbw/MorseProject/MorseProject.java
@@ -1,4 +1,15 @@
 package com.dhbw.MorseProject;
 
+import com.dhbw.MorseProject.send.Encoder;
+import com.dhbw.MorseProject.send.Melody;
+
+import java.util.ArrayList;
+
 public class MorseProject {
+
+    public static void main(String[] args) {
+        Encoder e = Encoder.getInstance();
+        e.send("... --- ...", new Melody("test", new ArrayList<>()), 100);
+    }
+
 }
