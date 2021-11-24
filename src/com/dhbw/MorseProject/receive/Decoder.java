0a9d969f8d58ce2297aef9aac641a4ab57fef5c1
s200299 <s200299@student.dhbw-mannheim.de>
2021-11-24 16:30:40 +0100

    Added basic thread runnable without sample analysis

--- src/com/dhbw/MorseProject/receive/Decoder.java
+++ src/com/dhbw/MorseProject/receive/Decoder.java
@@ -6,24 +6,69 @@ public class Decoder {
     List<Token> tokenList;
     List<MorseSignal> signalList;
     AudioListener audioListener;
-    private boolean isRecording;
+    private volatile boolean isRecording;
+    private Thread decoderThread;
 
     private static Decoder instance = null;
+    private Thread ui_update_thread;
 
     private Decoder(){
 
     }
 
     public boolean startRecording(Thread ui_update_thread){
-        isRecording = true;
+        if (isRecording){
+            return false;
+        } else{
+            try {
+                isRecording = true;
 
-        return false;
+                this.ui_update_thread = ui_update_thread;
+
+                decoderThread = new Thread(decoderRunnable);    //Creating new Thread because you can only call .start on Thread once
+                decoderThread.start();
+
+                return true;
+            } catch (Exception e){
+                return false;
+            }
+        }
+
+
     }
 
     public boolean stopRecording(){
-        isRecording = false;
-        return false;
+
+        try{
+            isRecording = false;    //setting boolean to false for graceful finish of decoderThread
+            decoderThread.join();   //joining thread to wait until finish
+            return true;    //return true if success
+        } catch (InterruptedException ie){
+            return false;   //return false if failed
+        }
     }
+
+
+
+    private final Runnable decoderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            while (isRecording) {
+                //Thread.onSpinWait();
+                try {
+                    wait(); //wait until decoder is notified with new sample from audioListener
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                double sample[] = audioListener.getNewSample(); //getting new Sample from audioListener
+                //TODO: analyze sample
+                //... sample analysis
+
+                ui_update_thread.notify();  //notify ui_update_thread about new signal
+
+            }
+        }
+    };
 
     public boolean isRecording(){
         return this.isRecording;
