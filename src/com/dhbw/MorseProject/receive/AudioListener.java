914817bbaa2f881e3bb07986cf651b31ea66187e
s200299 <s200299@student.dhbw-mannheim.de>
2021-11-24 16:09:25 +0100

    Added basic Thread skeleton for multithreading

--- src/com/dhbw/MorseProject/receive/AudioListener.java
+++ src/com/dhbw/MorseProject/receive/AudioListener.java
@@ -4,12 +4,33 @@ import javax.sound.sampled.TargetDataLine;
 
 public class AudioListener {
 
+
+
     private TargetDataLine line = null;
+    private boolean isListening = false;
 
     public void startListening(){
-
-
-
+        isListening = true; //boolean auf true setzen für schleifendurchlauf in listenerThread
+        listenerThread.start(); //listenerThread in neuem Thread starten
     }
+
+    public void stopListening(){
+        isListening = false;    //boolean auf false setzen, damit schleife den durchlauf demnächst sauber beendet
+        try {
+            listenerThread.join();  //join, um zu warten bis listenerThread fertig ist.
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    Thread listenerThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while(isListening){
+                //...
+            }
+        }
+    });
+
 
 }
