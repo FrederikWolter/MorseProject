f086d4f245f9d695597983a169c8ec252ffefb92
s200299 <s200299@student.dhbw-mannheim.de>
2021-11-24 15:52:39 +0100

    created method skeletons

--- src/com/dhbw/MorseProject/receive/Decoder.java
+++ src/com/dhbw/MorseProject/receive/Decoder.java
@@ -1,5 +1,29 @@
 package com.dhbw.MorseProject.receive;
 
-public class Decoder {
+import java.util.List;
 
+public class Decoder {
+    List<Token> tokenList;
+    List<MorseSignal> signalList;
+    AudioListener audioListener;
+    private boolean isRecording;
+
+    public boolean startRecording(Thread ui_update_thread){
+        isRecording = true;
+
+        return false;
+    }
+
+    public boolean stopRecording(){
+        isRecording = false;
+        return false;
+    }
+
+    public boolean isRecording(){
+        return this.isRecording;
+    }
+
+    public String getLastSignal(){
+        return "";
+    }
 }
