4d99c93aa2a0bcee06e36fa27a00d6de91094e62
Daniel Czeschner <daniel.czeschner@web.de>
2021-12-01 22:04:25 +0100

    Windowed Buffer Added. Still not ready to use!!!

--- src/com/dhbw/MorseProject/receive/MorseSignal.java
+++ src/com/dhbw/MorseProject/receive/MorseSignal.java
@@ -4,5 +4,28 @@ package com.dhbw.MorseProject.receive;
  * @author Daniel Czehscner
  */
 public class MorseSignal {
-    //TODO
+
+    private boolean quiet;
+    private long lengthInMs;
+
+    public MorseSignal(boolean quiet, long lengthInMs) {
+        this.quiet = quiet;
+        this.lengthInMs = lengthInMs;
+    }
+
+    public boolean isQuiet() {
+        return quiet;
+    }
+
+    public void setQuiet(boolean quiet) {
+        this.quiet = quiet;
+    }
+
+    public long getLengthInMs() {
+        return lengthInMs;
+    }
+
+    public void setLengthInMs(long lengthInMs) {
+        this.lengthInMs = lengthInMs;
+    }
 }
