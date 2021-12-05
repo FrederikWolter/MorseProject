bc0fe7815fb21ab90d1555f9bd98818e05f023a9
s200299 <s200299@student.dhbw-mannheim.de>
2021-12-05 19:28:52 +0100

    make start sending Button switch

--- src/com/dhbw/MorseProject/gui/GUI.java
+++ src/com/dhbw/MorseProject/gui/GUI.java
@@ -26,8 +26,8 @@ public class GUI {
     private JButton receive_clear_button;
     private JSplitPane receiveSplitPane;
     private JSplitPane sendSplitPane;
-    private boolean showStartRecording = true;
-    private boolean showBeginSend = true;
+    private boolean showingStartRecording = true;
+    private boolean showingBeginSend = true;
 
     public GUI(){
         JFrame frame = new JFrame("GUI-Test");
@@ -67,12 +67,26 @@ public class GUI {
 
                 //TODO link receive module
 
-                if (showStartRecording){
+                if (!showingStartRecording){
                     startRecordingButton.setText("Start Recording");
                 } else{
                     startRecordingButton.setText("Stop Recording");
                 }
-                showStartRecording = !showStartRecording;
+                showingStartRecording = !showingStartRecording;
+            }
+        });
+
+        beginSendingButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO link receive module
+
+                if (!showingBeginSend){
+                    beginSendingButton.setText("Senden beginnen");
+                } else{
+                    beginSendingButton.setText("Senden beenden");
+                }
+                showingBeginSend = !showingBeginSend;
             }
         });
     }
