32fb1710e048a8e76fcb9c50a0124c5347d4f017
s200299 <s200299@student.dhbw-mannheim.de>
2021-12-08 18:08:33 +0100

    added Events and listeners for Errors

---
+++ src/com/dhbw/MorseProject/send/EncoderErrorEvent.java
@@ -0,0 +1,19 @@
+package com.dhbw.MorseProject.send;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EncoderErrorEvent {
+    private List<IEncoderErrorListener> listeners = new ArrayList<IEncoderErrorListener>();
+
+    public void addListener(IEncoderErrorListener listener){
+        listeners.add(listener);
+    }
+
+    public void alert(String message){
+        for (IEncoderErrorListener listener:
+                listeners) {
+            listener.run(message);
+        }
+    }
+}
---
+++ src/com/dhbw/MorseProject/send/IEncoderErrorListener.java
@@ -0,0 +1,5 @@
+package com.dhbw.MorseProject.send;
+
+public interface IEncoderErrorListener {
+    void run(String message);
+}
