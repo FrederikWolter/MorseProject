3abcbc5fbc70ac77b3707520bbe5ddae1fbd3bc0
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2021-12-09 16:51:49 +0100

    Comments in IEncoderErrorListener.java and IEncoderFinishedListener.java

--- src/com/dhbw/MorseProject/send/events/IEncoderFinishedListener.java
+++ src/com/dhbw/MorseProject/send/events/IEncoderFinishedListener.java
@@ -1,5 +1,12 @@
 package com.dhbw.MorseProject.send.events;
 
+/**
+ * Interface used for Listener to 'encoder finished'-event.
+ * @author Mark Mühlenbarg & Frederik Wolter
+ */
 public interface IEncoderFinishedListener {
+    /**
+     * Method executed when event is triggered.
+     */
     void run();
 }
