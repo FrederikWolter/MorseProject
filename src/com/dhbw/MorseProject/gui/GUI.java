29e2bc2c7cdf559b6a3da5106a706c6fa6d944c7
s200299 <s200299@student.dhbw-mannheim.de>
2021-12-09 17:53:03 +0100

    added clearing of text areas before receiving

--- src/com/dhbw/MorseProject/gui/GUI.java
+++ src/com/dhbw/MorseProject/gui/GUI.java
@@ -168,6 +168,8 @@ public class GUI {
 
                 } else{
 
+                    clear_textAreas(receive_text_textArea, receive_morse_textArea); //Clearing text areas beforehand
+
                     Runnable ui_update_runnable = () -> {
                         do {
                             try {
