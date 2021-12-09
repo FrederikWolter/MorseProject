32cfd41f4f032be8d54e4023321bacf8407d3a8e
s200299 <s200299@student.dhbw-mannheim.de>
2021-12-09 18:24:24 +0100

    refactoring - extracting code to methods to improve readability and understandability of procedural code

--- src/com/dhbw/MorseProject/gui/GUI.java
+++ src/com/dhbw/MorseProject/gui/GUI.java
@@ -75,131 +75,35 @@ public class GUI {
         /**
          * Settings for frame and application appearance
          */
-        JFrame frame = new JFrame("Kommunikation via Morsecode - Technikmuseum Kommunikatioinstechnik München");
-        frame.add(mainpanel);
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        //frame.setResizable(false);
+        JFrame frame = setupJFrame();
 
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                showingStartRecording = false;
-                frame.dispose();
-                System.exit(0);
-            }
-        });
 
-        try {
-            frame.setIconImage(ImageIO.read(new File("src/com/dhbw/MorseProject/gui/Morse_Symbolbild.png")));
-            // Quelle: "https://w7.pngwing.com/pngs/27/465/png-transparent-morse-code-computer-icons-communication-others-text-code-morse-code.png"
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        prepareInfoPanel();
 
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(frame.getGraphicsConfiguration());
-        int taskBarSize = scnMax.bottom;
-        int effectiveMaxHeight = (screenSize.height-taskBarSize);
+        prepareMelodySelectionComboBox();
 
-        double scale = 0.5;
-        double modifier = (int) (1.0/scale);
-        Dimension windowSize = new Dimension((int)(((double) screenSize.width)/modifier), (int)(((double)effectiveMaxHeight)/modifier));
-        frame.setMinimumSize(windowSize);
-        mainpanel.setMinimumSize(windowSize);
+        configureListeners();
 
-        double multiplier = 1 + 1.0/modifier;
-        frame.setLocation(screenSize.width - (int) (frame.getWidth()*multiplier), effectiveMaxHeight - (int) (frame.getHeight()*multiplier) );
+    }
 
-        adjust_splitpane_sizes(sendSplitPane, send_text_textArea, send_morse_textArea);
-
-        adjust_splitpane_sizes(receiveSplitPane, receive_text_textArea, receive_morse_textArea);
-
+    private void configureListeners() {
         /**
-         * Filling text and table in Information-Tab with content and setting appearance
-         * @see #fillTable()
-         */
-        String[][] data = fillTable();
-        String[] columnNames = {"Schriftzeichen", "Morse-Code"};
-        DefaultTableModel tableModel = (DefaultTableModel) table_alphabet.getModel();
-        tableModel.addColumn(columnNames[0]);
-        tableModel.addColumn(columnNames[1]);
-        for (String[] datum : data) {
-            tableModel.addRow(new Object[]{datum[0], datum[1]});
-        }
-        DefaultTableCellRenderer centerRendering = new DefaultTableCellRenderer();
-        centerRendering.setHorizontalAlignment(JLabel.CENTER);
-        table_alphabet.getColumnModel().getColumn(0).setCellRenderer(centerRendering);
-        table_alphabet.getColumnModel().getColumn(1).setCellRenderer(centerRendering);
-
-        String info = """
-                Der Morsecode (auch Morsealphabet oder Morsezeichen genannt) ist ein gebräuchlicher Code zur telegrafischen Übermittlung von Buchstaben, Ziffern und weiterer Zeichen. Er bestimmt das Zeitschema, nach dem ein diskretes Signal ein- und ausgeschaltet wird.
-                
-                Der Code kann als Tonsignal, als Funksignal, als elektrischer Puls mit einer Morsetaste über eine Telefonleitung, mechanisch oder optisch (etwa mit blinkendem Licht) übertragen werden – oder auch mit jedem sonstigen Medium, mit dem zwei verschiedene Zustände (wie etwa Ton oder kein Ton) eindeutig und in der zeitlichen Länge variierbar dargestellt werden können. Dieses Übertragungsverfahren nennt man Morsetelegrafie.
-                
-                Das manchmal bei Notfällen beschriebene Morsen durch Klopfen an metallischen Verbindungen erfüllt diese Forderung daher nur bedingt, ist aber mit einiger Übung aufgrund des charakteristischen Rhythmus von Morsezeichen verständlich. Es ist abgeleitet von den „Klopfern“ aus der Anfangszeit der Telegrafentechnik, bestehend aus einem Elektromagneten mit Anker in einem akustischen Hohlspiegel. Beim Einschalten erzeugte er ein lautes und beim Abschalten ein etwas leiseres Klopfgeräusch. So konnte man den Klang der Morsezeichen schon vor der Erfindung des Lautsprechers selbst in größeren Betriebsräumen hörbar machen.
-                
-                
-                Auszug aus de.wikipedia.org/wiki/Morsecode
-                """;
-        //Quelle: https://de.wikipedia.org/wiki/Morsecode
-        textArea_info.setText(info);
-
-        /**
-         * Action-Listener to start the recording of audio
+         * Action-Listener to start the recording of audio when startRecordingButton is pressed
          */
         startRecordingButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                //TODO link receive module
-
                 if (!showingStartRecording){
-                    boolean success = Decoder.getInstance().stopRecording();
-
-                    if(success){
-                        startRecordingButton.setText("Start Recording");
-                        showingStartRecording = !showingStartRecording;
-                    } else {
-                        showMessageDialog(null, "Es ist ein Fehler aufgetreten, bitte melden Sie sich beim Personal"+e, "Error", JOptionPane.ERROR_MESSAGE);
-                    }
+                    stopRecording(e);
 
                 } else{
-
-                    clear_textAreas(receive_text_textArea, receive_morse_textArea); //Clearing text areas beforehand
-
-                    Runnable ui_update_runnable = () -> {
-                        do {
-                            try {
-                                synchronized (ui_update_thread){
-                                    ui_update_thread.wait();
-                                }
-                            } catch (InterruptedException ex) {
-                                ex.printStackTrace();
-                            }
-                            receive_morse_textArea.append(Decoder.getInstance().getLastSignal());
-                            String currentMorseTranslation = Translator.morseToText(receive_morse_textArea.getText());
-                            receive_text_textArea.setText(currentMorseTranslation);
-                        } while (!showingStartRecording);
-                    };
-
-                    ui_update_thread = new Thread(ui_update_runnable);
-
-                    boolean success = Decoder.getInstance().startRecording(ui_update_thread);
-                    if (success){
-                        ui_update_thread.start();
-                        startRecordingButton.setText("Stop Recording");
-                        showingStartRecording = !showingStartRecording;
-                    } else {
-                        showMessageDialog(null, "Es ist ein Fehler aufgetreten, bitte melden Sie sich beim Personal"+e, "Error", JOptionPane.ERROR_MESSAGE);
-                    }
+                    startRecording(e);
                 }
             }
         });
 
         /**
-         * Action-Listener to start and stop sending the given morse-code via audio.
+         * Action-Listener to start and stop sending the given morse-code via audio when beginSendingButton is pressed
          * @see #startPlaying()
          * @see #stopPlaying()
          */
@@ -218,19 +122,6 @@ public class GUI {
             @Override
             public void run() {
                 stopPlayingChangeVariables();
-            }
-        });
-
-        /**
-         * Settings for the ComboBox to select given melodies or frequencies.
-         */
-        for (int i = 0; i < Melody.getMelodyList().size(); i++){
-            comboBox1.addItem(Melody.getMelodyList().get(i).getName());
-        }
-        comboBox1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frequenz_slider.setEnabled(comboBox1.getSelectedItem().toString().equals("Fest"));
             }
         });
 
@@ -280,7 +171,154 @@ public class GUI {
                 translateSendTextAreas(textAreaFocusMap);
             }
         });
+    }
 
+    private void prepareMelodySelectionComboBox() {
+        /**
+         * Settings for the ComboBox to select given melodies or frequencies.
+         */
+        for (int i = 0; i < Melody.getMelodyList().size(); i++){
+            comboBox1.addItem(Melody.getMelodyList().get(i).getName());
+        }
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frequenz_slider.setEnabled(comboBox1.getSelectedItem().toString().equals("Fest"));
+            }
+        });
+    }
+
+    private void prepareInfoPanel() {
+        /**
+         * Filling text and table in Information-Tab with content and setting appearance
+         * @see #fillTable()
+         */
+        String[][] data = fillTable();
+        String[] columnNames = {"Schriftzeichen", "Morse-Code"};
+        DefaultTableModel tableModel = (DefaultTableModel) table_alphabet.getModel();
+        tableModel.addColumn(columnNames[0]);
+        tableModel.addColumn(columnNames[1]);
+        for (String[] datum : data) {
+            tableModel.addRow(new Object[]{datum[0], datum[1]});
+        }
+        DefaultTableCellRenderer centerRendering = new DefaultTableCellRenderer();
+        centerRendering.setHorizontalAlignment(JLabel.CENTER);
+        table_alphabet.getColumnModel().getColumn(0).setCellRenderer(centerRendering);
+        table_alphabet.getColumnModel().getColumn(1).setCellRenderer(centerRendering);
+
+        String info = getInfoText();
+
+        textArea_info.setText(info);
+    }
+
+    private JFrame setupJFrame() {
+        JFrame frame = new JFrame("Kommunikation via Morsecode - Technikmuseum Kommunikatioinstechnik München");
+        frame.add(mainpanel);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        //frame.setResizable(false);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {  //overwriting the standard behaviour when closing the window
+                try{
+                    stopPlaying();
+                    stopRecording(null);
+                } catch (Exception exception){
+                    exception.printStackTrace();    //TODO: show error message when stopping failed
+                }
+                frame.dispose();
+                System.exit(0);
+            }
+        });
+
+        try {
+            frame.setIconImage(ImageIO.read(new File("src/com/dhbw/MorseProject/gui/Morse_Symbolbild.png")));
+            // Quelle: "https://w7.pngwing.com/pngs/27/465/png-transparent-morse-code-computer-icons-communication-others-text-code-morse-code.png"
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(frame.getGraphicsConfiguration());
+        int taskBarSize = scnMax.bottom;
+        int effectiveMaxHeight = (screenSize.height-taskBarSize);
+
+        setWindowDimensions(frame, screenSize, effectiveMaxHeight);
+
+        adjust_splitpane_sizes(sendSplitPane, send_text_textArea, send_morse_textArea);
+
+        adjust_splitpane_sizes(receiveSplitPane, receive_text_textArea, receive_morse_textArea);
+
+        return frame;
+    }
+
+    private void setWindowDimensions(JFrame frame, Dimension screenSize, int effectiveMaxHeight) {
+        double scale = 0.5;
+        double modifier = (int) (1.0/scale);
+        Dimension windowSize = new Dimension((int)(((double) screenSize.width)/modifier), (int)(((double) effectiveMaxHeight)/modifier));
+        frame.setMinimumSize(windowSize);
+        mainpanel.setMinimumSize(windowSize);
+
+        double multiplier = 1 + 1.0/modifier;
+        frame.setLocation(screenSize.width - (int) (frame.getWidth()*multiplier), effectiveMaxHeight - (int) (frame.getHeight()*multiplier) );
+    }
+
+    private String getInfoText() {
+        return """
+                Der Morsecode (auch Morsealphabet oder Morsezeichen genannt) ist ein gebräuchlicher Code zur telegrafischen Übermittlung von Buchstaben, Ziffern und weiterer Zeichen. Er bestimmt das Zeitschema, nach dem ein diskretes Signal ein- und ausgeschaltet wird.
+                
+                Der Code kann als Tonsignal, als Funksignal, als elektrischer Puls mit einer Morsetaste über eine Telefonleitung, mechanisch oder optisch (etwa mit blinkendem Licht) übertragen werden – oder auch mit jedem sonstigen Medium, mit dem zwei verschiedene Zustände (wie etwa Ton oder kein Ton) eindeutig und in der zeitlichen Länge variierbar dargestellt werden können. Dieses Übertragungsverfahren nennt man Morsetelegrafie.
+                
+                Das manchmal bei Notfällen beschriebene Morsen durch Klopfen an metallischen Verbindungen erfüllt diese Forderung daher nur bedingt, ist aber mit einiger Übung aufgrund des charakteristischen Rhythmus von Morsezeichen verständlich. Es ist abgeleitet von den „Klopfern“ aus der Anfangszeit der Telegrafentechnik, bestehend aus einem Elektromagneten mit Anker in einem akustischen Hohlspiegel. Beim Einschalten erzeugte er ein lautes und beim Abschalten ein etwas leiseres Klopfgeräusch. So konnte man den Klang der Morsezeichen schon vor der Erfindung des Lautsprechers selbst in größeren Betriebsräumen hörbar machen.
+                
+                
+                Auszug aus de.wikipedia.org/wiki/Morsecode
+                """;
+
+        //Quelle: https://de.wikipedia.org/wiki/Morsecode
+    }
+
+    private void stopRecording(ActionEvent e) {
+        boolean success = Decoder.getInstance().stopRecording();
+
+        if(success){
+            startRecordingButton.setText("Start Recording");
+            showingStartRecording = !showingStartRecording;
+        } else {
+            showMessageDialog(null, "Es ist ein Fehler aufgetreten, bitte melden Sie sich beim Personal"+ e, "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void startRecording(ActionEvent e) {
+        clear_textAreas(receive_text_textArea, receive_morse_textArea); //Clearing text areas beforehand
+
+        Runnable ui_update_runnable = () -> {
+            do {
+                try {
+                    synchronized (ui_update_thread){
+                        ui_update_thread.wait();
+                    }
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                receive_morse_textArea.append(Decoder.getInstance().getLastSignal());
+                String currentMorseTranslation = Translator.morseToText(receive_morse_textArea.getText());
+                receive_text_textArea.setText(currentMorseTranslation);
+            } while (!showingStartRecording);
+        };
+
+        ui_update_thread = new Thread(ui_update_runnable);
+
+        boolean success = Decoder.getInstance().startRecording(ui_update_thread);
+        if (success){
+            ui_update_thread.start();
+            startRecordingButton.setText("Stop Recording");
+            showingStartRecording = !showingStartRecording;
+        } else {
+            showMessageDialog(null, "Es ist ein Fehler aufgetreten, bitte melden Sie sich beim Personal"+ e, "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     private void startPlaying() {
