8baf255c15c79e9538cd75d927955cdc4c51cbaf
Daniel Czeschner <daniel.czeschner@web.de>
2022-01-01 13:50:17 +0100

    Noise is now a record class

--- src/com/dhbw/morse_project/receive/Noise.java
+++ src/com/dhbw/morse_project/receive/Noise.java
@@ -1,28 +1,14 @@
 package com.dhbw.morse_project.receive;
 
 /**
- * This class is used as a POJO for the representation of one Sample.
+ * This (record)class is used as a POJO for the representation of one Sample.
  *
  * @author Mark Mühlenberg, mininaml changes by Daniel Czeschner
  * @see AudioListener
+ * @param quiet Ture if this sample is silence. (Smaller than the threshold (see {@link AudioListener}))
+ * @param index The index of this Sample.
  */
-public class Noise {
-    // TODO convert to java record recommend; similar to data class in kotlin https://jax.de/blog/datenklassen-in-java-einfuehrung-in-java-records/
-    // region POJO attributes
-    private final boolean quiet;
-    private final int index;
-    //endregion
-
-    /**
-     * Constructor of {@link Noise} Class.
-     *
-     * @param quiet Ture if this sample is silence. (Smaller than the threshold (see {@link AudioListener}))
-     * @param index The index of this Sample.
-     */
-    public Noise(boolean quiet, int index) {
-        this.quiet = quiet;
-        this.index = index;
-    }
+public record Noise(boolean quiet, int index) {
 
     // region getter
     public int getIndex() {
